5f477535068b1f6da5e532e5792860ccffab1177
xFruts <keksikiforlove@outlook.com>
2025-09-19 17:45:50 +0300

    feat: Initialize Spring Boot with gradle

---
+++ src/main/java/ru/maxow/massistant/MAssistantApplication.java
@@ -0,0 +1,13 @@
+package ru.maxow.massistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MAssistantApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MAssistantApplication.class, args);
+  }
+
+}
